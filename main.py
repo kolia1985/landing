0d940a7f3e2f3185fd19cc9491ddd54583d1c229
Nikolay Melnik <nmelnik@odesk.com>
2015-11-10 05:04:04 +0200

    Added base template

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def landingpage():
+    return render_template('landing.html')
+
