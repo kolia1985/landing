83a92a2cc507bc8bc4fa3875876a8c6f42443e56
Nikolay Melnik <nmelnik@odesk.com>
2015-11-11 13:01:00 +0200

    Updating agency profiles section

--- main.py
+++ main.py
@@ -4,7 +4,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def landingpage():
-    return render_template('landing.html')
-
+    agencies = [
+        {
+            'name': 'Ergonized',
+            'logo': 'https://odesk-prod-portraits.s3.amazonaws.com/Companies:1046958:CompanyLogoURL?AWSAccessKeyId=1XVAX3FNQZAFC9GJCFR2&Expires=2147483647&Signature=5Pc4IlYtySpTBPzdHA2kEs9jorA%3D&1447238772',
+            'job_success': '100',
+            'description': 'php development, WordPress, Yii, Symphony, Mobile Applications',
+            'total_jobs': 256,
+            'location': 'Ukraine',
+            'profile_url': 'https://www.upwork.com/companies/~01a5ddd182baca1686',
+            'review': 'The company has done a superb work for us. The engineers we dealt with are top-notch professionals.',
+            'skills': ['php', 'WordPress']
+        }
+    ] * 4
+    context = {'agencies': agencies}
+    return render_template('landing.html', **context)
